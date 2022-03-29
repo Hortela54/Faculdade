bece1d0550b41212d858eab4de87c82d79527027
Hortela54 <88990539+Hortela54@users.noreply.github.com>
2022-03-28 21:58:07 -0300

    Update trabalho004.py

--- Trabalho - Logica de Programação e Algortimos/trabalho004.py
+++ Trabalho - Logica de Programação e Algortimos/trabalho004.py
@@ -1,22 +1,44 @@
-def cadastrarPeca(cod):
+def cadastrarPeca(cod): 
+    print('Código da peça: {}'.format(cod))
+    pecas = {'cod':[cod],'nome':[],'fabricante':[], 'valor':[]}
+    nomePeca = input('Digite o nome da peça ') 
+    fabricantePeca = input('Digite o nome do fabricante da peça ')
+    valorPeca = int(input('Digite o valor da peça '))
+    pecas['nome'].append(nomePeca)
+    pecas['fabricante'].append(fabricantePeca)
+    pecas['valor'].append(valorPeca)
+    pecas.clear
+    return pecas
+
+#def consultarPeca():
+
+#def consultarPeca():
+
+#def removerPeca()
+
+def menuPrincipal():
+    print('Bem vindo ao Controle de Estoque de Bike Shop do Samuel Nilton da Silva(3892265)')
+    codigo = 0 
     while True:
-        pecas = {'cod':[cod],'nome':[],'fabricante':[], 'valor':[]}
-        nomePeca = input('Digite o nome da peça ') 
-        fabricantePeca = input('Digite o nome do fabricante da peça ')
-        valorPeca = float(input('Digite o valor da peça '))
-        pecas['nome'].append(nomePeca)
-        pecas['fabricante'].append(fabricantePeca)
-        pecas['valor'].append(valorPeca)
-        pecas.clear
-        break
-              
-        
+        print('1 - Cadastrar peças ')
+        print('2 - Consultar peças ')
+        print('3 - Remover peças ')
+        print('4 - Sair ')
+        res = int(input('Escolho a opção: '))
 
-#def consultarPeca():
+        if res == 1:
+            codigo += 1
+            pecas = cadastrarPeca(codigo)
+            print(pecas)
+        elif res == 2:
+            print('2')
+        elif res == 3:
+            print('3')
+        elif res == 4:
+            break
+    
 
-#def consultarPeca():
+def iniciar(): 
+    menuPrincipal()
 
-#def removerPeca():
-
-
-cadastrarPeca(1)
+iniciar()

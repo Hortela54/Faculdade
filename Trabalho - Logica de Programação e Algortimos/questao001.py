0ceb1eab28d4b140492137b530decf37b69571ce
Hortela54 <88990539+Hortela54@users.noreply.github.com>
2022-03-30 14:25:06 -0300

    Update questao001.py

--- Trabalho - Logica de Programação e Algortimos/questao001.py
+++ Trabalho - Logica de Programação e Algortimos/questao001.py
@@ -1,22 +1,22 @@
 print('Bem Vindo a Loja do Samuel(3892265)') # RU: 3892265
 valor = float(input("Digite o valor do produto: ")) # Valor do produto
-qtd = float(input("Digite a quantidade de produtos: ")) # Quantidade de produtos
+qtd3892265 = float(input("Digite a quantidade de produtos: ")) # Quantidade de produtos
 
-if qtd <= 9:
+if qtd3892265 <= 9:
     print('O valor sem desconto é {:.2f}'.format(valor)) # Valor até nove os produtos é 0% de desconto
     
-elif qtd >= 10 and qtd < 99:
-    valortotal = valor*qtd #Calculo do valor total dos produtos vezes a quantidade
+elif qtd3892265 >= 10 and qtd3892265 < 99:
+    valortotal = valor*qtd3892265 #Calculo do valor total dos produtos vezes a quantidade
     desconto = valortotal-(valortotal*0.05) #Aplicando o desconto em cima do valor total da compra
     print('O valor sem desconto foi: {}\nO Valor com desconto foi: {}'.format(valortotal, desconto))
     
-elif qtd >= 100 and qtd < 999:    
-    valortotal = valor*qtd
+elif qtd3892265 >= 100 and qtd3892265 < 999:    
+    valortotal = valor*qtd3892265
     desconto = valortotal-(valortotal*0.10)
     print('O valor sem desconto foi: {}\nO Valor com desconto foi: {}'.format(valortotal, desconto))
     
-elif qtd > 1000:  
-    valortotal = valor*qtd
+elif qtd3892265 > 1000:  
+    valortotal = valor*qtd3892265
     desconto = valortotal-(valortotal*0.15)
     print('O valor sem desconto foi: {}\nO Valor com desconto foi: {}'.format(valortotal, desconto))
     
